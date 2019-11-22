f1587b26c20a447c082c36aefe93154ee2db6273
alberthor47 <albertocamarena.77t@gmail.com>
2019-11-21 18:26:26 -0600

    Listo la pagina de 404

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../assets/styles/components/NotFound.scss';
 
 const NotFound = () => (
-  <>
-    <h1>404 No encontrado</h1>
-  </>
+  <div className="page404">
+    <div className="page404_container">
+      <div className="page404_img">
+        <img src="https://static.platzi.com/static/images/error/img404.a1003ad86732.png" alt="notFound"/>
+      </div>
+      <div className="page404_text">
+        <h3>Mmmm...</h3>
+        <p>No encontramos lo que buscabas, sorry! 💩</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default NotFound;
